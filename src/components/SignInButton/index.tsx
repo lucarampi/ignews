458b5d25277e99059ddcf53c6e8c1df93978931b
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-04-06 15:51:46 -0300

    added login

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,21 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const { data: session } = useSession();
 
-console.log(session)
+  console.log(session);
 
   return session ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Luca A. Rampinelli
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
